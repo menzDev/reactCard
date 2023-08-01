2c207433f320aedef153b46231a265efc1a2ec39
carlosllinas <112914013+menzDev@users.noreply.github.com>
2023-07-31 22:46:36 -0500

    31/07/23

--- src/Evento.tsx
+++ src/Evento.tsx
@@ -1,47 +1,53 @@
-import './App.css'
-import Rey1 from "../src/assets/rey_recesvinto.png";
-import Rey2 from "../src/assets/rey_teodorico.png";
+import "./App.css";
+import reysito from "./assets/rey_atanagildo.png";
+import reyII from "./assets/rey_teodorico.png";
 import { useRef } from "react";
 
 function Evento() {
-    const cambio = 4
-    const referencia = useRef();
-    const valorNumerico =(e)=>{
-        e.target.innerHTML = Number(e.target.innerHTML) + 1
-        if(e.target.innerHTML >= 10 ){
-            e.target.innerHTML = 1
-        }
-        if(e.target.innerHTML >=8){
-        e.target.style.backgroundColor = "red"
-    }else{
-        e.target.style.backgroundColor = "blue"
-    }
+  const referencia = useRef();
+  let moneda = 4;
+
+  const valor = () => {
+    referencia.current.innerHTML = Number(referencia.current.innerHTML) * moneda
+  }
+
+  const aumento = (e) => {
+    e.target.innerHTML = Number(e.target.innerHTML) + 1;
+    if(e.target.innerHTML > 8){
+e.target.style.backgroundColor = "blue"
+} if (e.target.innerHTML > 9) {
+    e.target.innerHTML = 1
+    e.target.style.backgroundColor = "goldenrod"
+} else {
+    
 }
-    const cambiarValor = ()=>{
-        referencia.current.innerHTML = Number(referencia.current.innerHTML )* cambio
+  };
+
+  const captar = (c) =>{
+    referencia.current.innerHTML = c.target.value
+  }
+
+  const cambiarRey =(i) =>{
+      if(i.target.src.includes("atanagildo")){
+        i.target.src = reyII
+    } else{
+        i.target.src = reysito
     }
-    const cambioInput = (e) =>{
-        referencia.current.innerHTML = e.target.value 
-        
-    }
-    const cambiarRey = (e) =>{
-        if (e.target.src.includes("recesvinto")) {
-            e.target.src = Rey2
-        }else{
-            e.target.src = Rey1
-    }
-}
-    return(
-        <>
-        <div className="contenedorEvento">
-            <p className='p' ref={referencia} onClick={valorNumerico}>1</p>
-            <button onClick={cambiarValor}>Cambiar Valor</button>
-            <img onClick={cambiarRey} src={Rey1} />
-            <input onChange={cambioInput} type="text" />
+  }
+  return (
+    <>
+      <div className="contenedor">
+        <div className="carta">
+          <p ref={referencia} onClick={aumento} className="p">
+            1
+          </p>
+          <button onClick={valor}>click</button>
+          <img onClick={cambiarRey} src={reysito} alt="" />
+          <input onChange={captar} type="text" />
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
 
-
-export default Evento
+export default Evento;
